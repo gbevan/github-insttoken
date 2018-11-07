b37545aea062306e22eb545ac1438e9f3d9d854d
Graham Bevan <graham.bevan@ntlworld.com>
2018-11-07 09:06:52 +0000

    debugging proxy support

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"strconv"
 	"time"
@@ -69,17 +70,30 @@ func main() {
 		panic(err)
 	}
 
-	// fmt.Println(tokenString)
-
-	client := &http.Client{}
+	// // fmt.Println(tokenString)
+	// tr := &http.Transport{
+	// 	Proxy: http.ProxyFromEnvironment,
+	// 	DialContext: (&net.Dialer{
+	// 		Timeout:   30 * time.Second,
+	// 		KeepAlive: 30 * time.Second,
+	// 		DualStack: true,
+	// 	}).DialContext,
+	// 	MaxIdleConns:          100,
+	// 	IdleConnTimeout:       90 * time.Second,
+	// 	TLSHandshakeTimeout:   10 * time.Second,
+	// 	ExpectContinueTimeout: 1 * time.Second,
+	// }
+	// tr.ProxyConnectHeader = http.Header{}
+	// client := &http.Client{Transport: tr}
 
 	// Get Installation ID
 	insResp, err := reqGithub(
-		client,
+		// client,
 		"GET",
 		fmt.Sprintf("%v/repos/%v/installation", *gitURL, *repo),
 		nil,
 		jwt,
+		// tr,
 	)
 	if err != nil {
 		panic(err)
@@ -88,11 +102,12 @@ func main() {
 
 	// Get Installation Token
 	insTokResp, err := reqGithub(
-		client,
+		// client,
 		"POST",
 		fmt.Sprintf("%v/app/installations/%v/access_tokens", *gitURL, insResp["id"]),
 		nil,
 		jwt,
+		// tr,
 	)
 	if err != nil {
 		panic(err)
@@ -100,16 +115,63 @@ func main() {
 	fmt.Println("token:", insTokResp["token"])
 }
 
-func reqGithub(client *http.Client, method string, url string, postBody io.Reader, jwt string) (map[string]interface{}, error) {
+// func reqGithub(client *http.Client, method string, url string, postBody io.Reader, jwt string, tr *http.Transport) (map[string]interface{}, error) {
+func reqGithub(method string, url string, postBody io.Reader, jwt string) (map[string]interface{}, error) {
 	req, err := http.NewRequest(method, url, postBody)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwt))
+
+	tr := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		// DisableCompression:    true,
+	}
+	// tr.ProxyConnectHeader = http.Header{}
+
+	req.Header.Set("Host", "github.dxc.com")
+	// req.Header.Set("Host", "GitHub.com")
 	req.Header.Set("Accept", "application/vnd.github.machine-man-preview+json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwt))
+
+	// proxy
+	// req.Header.Set("Proxy-Connection", "Keep-Alive")
+	// tr.ProxyConnectHeader.Set("Proxy-Connection", "Keep-Alive")
+
+	// force user-agent for proxy
+	// req.Header.Set("User-Agent", "Wget/1.9.1")
+	// tr.ProxyConnectHeader = req.Header
+
+	client := &http.Client{Transport: tr}
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		if len(via) > 15 {
+			return fmt.Errorf("%d consecutive redirects", len(via))
+		}
+		if len(via) == 0 {
+			return nil
+		}
+		for key, val := range via[0].Header {
+			req.Header[key] = val
+		}
+		fmt.Printf("Redirect header: %v\n", req.Header)
+		return nil
+	}
+	// fmt.Printf("req: %v\n", req)
+	fmt.Printf("req.Header: %v\n", req.Header)
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
+	}
+	if resp.StatusCode != 200 && resp.StatusCode != 201 {
+		return nil, fmt.Errorf("Error: Invalid response from github, resp: %v", resp)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
